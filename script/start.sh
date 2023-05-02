527a2cfaf369e09a7bae84962efdd57ae74f59e7
matt <echoedinvoker@gmail.com>
2023-05-02 23:38:26 +0800

    refactor start.sh to move all ws0 windows to ws1 and create terminal and chrome of course on ws1

--- script/start.sh
+++ script/start.sh
@@ -9,8 +9,15 @@ else
     exit 1
 fi
 
+wmctrl -s 0
+wmctrl -l | awk '$2 == "0" {print $1}' | while read -r win_id; do
+    wmctrl -i -r "$win_id" -t 1
+done
+
 # Open a new terminal window and go to the target directory
 mkdir -p "$TARGET_DIR/pic"
 gnome-terminal --working-directory="$TARGET_DIR" -e "nvim memo.md"
-google-chrome "$TARGET_URL"
+google-chrome --new-window "$TARGET_URL"
+
+# wmctrl -l | grep -i "Udemy" | awk '{print $1}' | while read -r line | wmctrl -i -r "$line" -t 0 ## don't need any more, but this can be a reference
 
